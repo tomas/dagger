5106faea28c4744f357550c3cefcae7055a19458
Tomás Pollak <tomaspollak@gmail.com>
2020-03-31 11:39:25 -0300

    Add spec/ip_connect_spec.rb

---
+++ spec/ip_connect_spec.rb
@@ -0,0 +1,17 @@
+require './lib/dagger'
+
+require 'rspec/mocks'
+require 'rspec/expectations'
+
+describe 'IP Connection' do
+
+  it 'works' do
+    expect do
+      Dagger.get('http://www.awiefjoawijfaowef.com')
+    end.to raise_error(SocketError, /getaddrinfo/)
+
+    resp = Dagger.get('http://www.awiefjoawijfaowef.com', { ip: '1.1.1.1'} )
+    expect(resp.status).to eq(404)
+  end
+
+end
